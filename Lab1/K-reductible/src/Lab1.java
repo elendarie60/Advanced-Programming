b763a5d6c9feae9b3909870f0269da3be1bf381a
elendarie60 <79052848+elendarie60@users.noreply.github.com>
2024-03-03 09:27:27 -0900

    Update Lab1.java

--- Lab1/K-reductible/src/Lab1.java
+++ Lab1/K-reductible/src/Lab1.java
@@ -1,47 +1,73 @@
+import java.util.Scanner;
+
 public class Lab1 {
+    public static int computeSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static boolean isKReductible(int n, int k) {
+        while (true) {
+            n = computeSum(n);
+            if (n == k || n == 1) {
+                return true;
+            }
+            if (n < k || n == 89) { // Optimization to avoid infinite loops for non-reductible numbers
+                return false;
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        // Valid arg
         if (args.length != 3) {
-            System.out.println("java Lab1 <a> <b> <k>");
+            System.out.println("Usage: java Lab1 a b k");
             return;
         }
+
+        // Parse arg
         int a, b, k;
         try {
             a = Integer.parseInt(args[0]);
             b = Integer.parseInt(args[1]);
             k = Integer.parseInt(args[2]);
         } catch (NumberFormatException e) {
-            System.out.println("Invalid a sau b sau k");
+            System.out.println(" a, b, k invalid");
             return;
         }
+
         if (a > b) {
-            System.out.println("a trebuie sa fie mai mic sau egal cu b");
+            System.out.println("'a' > 'b' error");
             return;
         }
+
         StringBuilder result = new StringBuilder();
         long startTime = System.nanoTime();
-        for (int num = a; num <= b; num++) {
-            if (isKReductible(num, k)) {
-                result.append(num).append(" ");
+
+        for (int i = a; i <= b; i++) {
+            if (isKReductible(i, k)) {
+                result.append(i).append(" ");
             }
         }
         long endTime = System.nanoTime();
-        long duration = endTime - startTime;
-        System.out.println("Numerele kreductibile din interval [" + a + "," + b + "]" + result);
+        long duration = (endTime - startTime) / 1_000_000; // Duration in milliseconds
 
-        System.out.println("Timpul:" + duration + "nanosecunde");
+        //result and running time
+        System.out.println("K-reductible numbers in the interval [" + a + ", " + b + "] with k = " + k + ":");
+        System.out.println(result);
+        System.out.println("Running time: " + duration + " milliseconds");
     }
 
-    private static boolean isKReductible(int num, int k) {
-        while (num != k && num != 1) {
-            int sum = 0;
-            String digits = String.valueOf(num);
-            for (char digit : digits.toCharArray()) {
-                int d = Character.getNumericValue(digit);
-                sum += d * d;
-            }
-            num = sum;
+    public class WheelGraph {
+        public static int[][] createAdjacencyMatrix(int n) {
+            int[][] adjacencyMatrix = new int[n][n];
+            // TO DO 
+            return adjacencyMatrix;
         }
-        return num == k;
     }
-
 }
